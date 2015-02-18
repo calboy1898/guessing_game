4c266fca806c71c629237a3ebcfa3eb53c3f273a
Ryan Bronz <ryanbronz@Ryans-MacBook-Pro.local>
2015-02-18 17:35:35 -0500

    final draft 2

--- g_g.js
+++ g_g.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	var mynum=Math.floor((Math.random() * 100) + 1);
 	var guessarray=[];
 	var guesscount=0;
+	var hintrepeat=false;
 //larger function to call on either button click or return
 	var ggmain=function(){
 		var x=document.getElementById("numinput").value;
@@ -12,7 +13,7 @@ $(document).ready(function(){
 				dupguess=true;
 			}
 		}
-		if (guessnum%1===0&&guessnum>0&&guessnum<=100&&dupguess===false){
+		if (guessnum%1===0&&guessnum>0&&guessnum<=100&&dupguess===false&&hintrepeat===false){
 		guessarray.push(guessnum);
 		var diff=guessnum-mynum;
 		var newdiff=Math.abs(guessarray[guessarray.length-1]-mynum);
@@ -44,7 +45,7 @@ $(document).ready(function(){
 			$("body").append("<p>Guess: "+x+", you are "+hotcold+""+hottercolder+", "+higherlower+"!</p>");	
 		} else {
 			var hotcold="hot";
-			$("body").append("<p>Guess:"+x+", you are "+hotcold+""+hottercolder+", "+higherlower+"!</p>");
+			$("body").append("<p>Guess: "+x+", you are "+hotcold+""+hottercolder+", "+higherlower+"!</p>");
 		}
 		//$("body").append("<p>You guessed "+x+" and you are "+hotcold+"!</p>");
 		
@@ -57,12 +58,14 @@ $(document).ready(function(){
 		//$("body").append("<p>You guessed "+x+" and you are "+hotcold+"!</p>");
 		$('#numinput').val('');//.placeholder();
 		} else if (dupguess===true) {
-		alert("You have alredy guessed that number!");
-		$('#numinput').val('');//.placeholder();
+			alert("You have alredy guessed that number!");
+			$('#numinput').val('');//.placeholder();
 		//$("body").append("<p>You have already guessed that number!</p>");
+		} else if (hintrepeat===true){
+			alert("Please restart the game!");
 		} else {
-		alert("Please Enter a Valid Integer 1-100");
-		$("#numinput").val('');//.placeholder();
+			alert("Please Enter a Valid Integer 1-100");
+			$("#numinput").val('');//.placeholder();
 		//$("body").append("<p>Please Enter a Valid Integer 1-100</p>");
 		}
 	}
@@ -79,8 +82,11 @@ $(document).ready(function(){
 
 	// Defining action of "Give me a hint" button
 	$("#button2").on("click",function(){
-		var youlose=$("<p>The Correct Number is "+mynum+", Dumbass!<p/>");
-		$("body").append(youlose);
+		var youlose=$("<p>The Correct Number is "+mynum+"!<p/>");
+		if (hintrepeat===false){
+			$("body").append(youlose);
+			hintrepeat=true;
+		}
 	})
 
 	//defining action of "Play Again" button
@@ -90,6 +96,8 @@ $(document).ready(function(){
 		guessarray=[];
 		guesscount=0;
 		$("body").css({"background-color":"#444444"});
+		hintrepeat=false;
+		$("#numinput").val('');
 	})
 })
 
